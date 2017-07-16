5512fade8874512e3aa27c4c3464e0a19d71c6e2
agalecki <agalecki@umich.edu>
2017-07-16 15:18:59 -0400

    Update traceRedit.R
    fun_info () added
    traceReditor() modified

--- R/traceRedit.R
+++ R/traceRedit.R
@@ -1,36 +1,38 @@
-traceReditor <- function (fun, lbl, idx, verbose = FALSE)
-{    # function annotates fun with .traceR statements
-  if (verbose) message("-traceReditor: starts")
-  if (!is.null(attr(fun, "locked"))){
-  message ("Function: ", lbl,  " already annotated. No changes made.")
-  return(invisible(fun))
+fun_info <- function(fun, lbl = NULL ) {
+# Check whether body of a function is null
+ 
+ info <- character()
+ if (mode(fun) != "function")       info <- c(info, message ("Object", lbl, "is not a function))
+ if (is.null(body(fun)))            info <- c(info, message ("Function: ", lbl,  " body is null. No changes made."))
+ if (!is.null(attr(fun, "locked"))) info <- c(info,  message("Function: ", lbl,  " already annotated. No changes made.")
+ if (length(info > 0))    message ("Object/function ", lbl, "not suitable for annotations")
+ return(info)
 }
+
+
+traceReditor <- function (fun, lbl, idx, verbose = FALSE)
+{    
+  # function annotates fun with .traceR statements
+  if (verbose) message("-traceReditor: starts")
+ 
+ funinfo <- fun_info(fun, lbl= lbl) 
+ 
+ if (length(funinfo) > 0)  return(invisible(fun))  # Function unchanged
   
-curle_brackect_symbol <- as.symbol("{")
-if (verbose) message("traceReditor: before out0")
-out0 <- list(
-     quote(`{`),
-     substitute(.functionLabel <- tx, list(tx = lbl)),
-     quote(.traceR <- attr(options()$traceR, "fun")),
-     quote(.traceR <- if (is.null(.traceR)) function(...){} else .traceR)
-)
+ curle_brackect_symbol <- as.symbol("{")
 
-b_f <- body(fun)
+ b_f <- body(fun)
 
-if (is.null(b_f)) stop("Body of ", fun, " is NULL.") 
 
-if (verbose) message("traceReditor: before L")
-L <- if (is.symbol(b_f)){
-        # print(paste("-- is.symbol", lbl))
+ L <- if (is.symbol(b_f)){
         c(curle_brackect_symbol, b_f)
       } else {
         if (as.character(b_f[1]) != curle_brackect_symbol){
-          # print(paste("-- curle bracket", lbl))
           c(curle_brackect_symbol, b_f)
           } else {
           as.list(b_f)
       }}
-if (verbose) message("traceReditor: before T")          
+       
 T <- lapply(1:length(L), function(el){
         ix <- (idx *100 + el)/100
         lblx <- as.character(as.expression(L[[el]]))
@@ -39,14 +41,22 @@ T <- lapply(1:length(L), function(el){
         } else {
         substitute(.traceR(ix, lbl, auto = TRUE), list(ix = ix, lbl = lblx ))
         }})
-if (verbose) message("traceReditor: before out")    
+  
 out <- vector("list", 2 * length(L)-2 )
 for (i in seq_along(L)) {
   if (i > 1) out[2*i-2] <- L[i]
   if (i < length(L)) out[2*i-1]  <- T[i] 
 }
 
-if (verbose) message("traceReditor: before funR") 
+
+out0 <- list(
+     quote(`{`),
+     substitute(.functionLabel <- tx, list(tx = lbl)),
+     quote(.traceR <- attr(options()$traceR, "fun")),
+     quote(.traceR <- if (is.null(.traceR)) function(...){} else .traceR)
+)
+
+
 funR <- fun
 lx <- c(out0,out)
 body(funR) <- as.call(lx)
