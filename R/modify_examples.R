1eefa4fafbfbe06727a614de3de50a726c18194d
agalecki <agalecki@umich.edu>
2017-07-18 17:30:25 -0400

    Update modify_examples.R

--- R/modify_examples.R
+++ R/modify_examples.R
@@ -1,18 +1,20 @@
-traceReditor <- function (fun, lbl = ".", idx = 0, verbose = FALSE) {    
+traceReditor <- function (fun, flbl = deparse(substitute(fun)), idx = 0) {    
 # function annotates fun with .traceR statements
- if (verbose) message("-traceReditor: starts")
- curle_brackect_symbol <- as.symbol("{")
+# Returns an object of class "call"
+ 
  b_f <- body(fun)
+ if (is.null(b_f) || length(b_f) == 1) return(body(fun)) 
+ curle_brackect_symbol <- as.symbol("{")
  L <- if (is.symbol(b_f)  || as.character(b_f[1]) != curle_brackect_symbol) {
         c(curle_brackect_symbol, b_f)
       } else  as.list(b_f)       
-T <- lapply(1:length(L), function(el){
+ T <- lapply(1:length(L), function(el){
         ix <- (idx *100 + el)/100
         lblx <- as.character(as.expression(L[[el]]))
         if (el == 1) {   
-        substitute(.traceR(ix, lbl, first = TRUE, auto = TRUE), list(ix = ix, lbl = lblx))
+        substitute(.traceR(ix, flbl, first = TRUE, auto = TRUE), list(ix = ix, flbl = lblx))
         } else {
-        substitute(.traceR(ix, lbl, auto = TRUE), list(ix = ix, lbl = lblx ))
+        substitute(.traceR(ix, flbl, auto = TRUE), list(ix = ix, flbl = lblx ))
         }})
   
 out <- vector("list", 2 * length(L)-2 )
@@ -24,7 +26,7 @@ for (i in seq_along(L)) {
 ## Create new body
 out0 <- list(
      quote(`{`),
-     substitute(.functionLabel <- tx, list(tx = lbl)),
+     substitute(.functionLabel <- tx, list(tx = flbl)),
      quote(.traceR <- attr(options()$traceR, "fun")),
      quote(.traceR <- if (is.null(.traceR)) function(...){} else .traceR)
 )
